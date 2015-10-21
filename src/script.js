8fa661487ced8fb40108ac904ad6a586d8e22791
Cole Lyman <cole@colelyman.com>
2015-10-21 12:45:47 -0600

    Fixed stupid html mistakes in script.js

--- src/script.js
+++ src/script.js
@@ -1,4 +1,11 @@
-console.log("Hello");
+/* Feel free to discard this code, this just shows you some of the information
+that we have available to display.
+
+So pretty much we have all of the hymns and the speakers from october-2014
+General Conference. I couldn't find more recent sessions in JSON format, and
+unfortunately we don't have the text, but we have a link at least.
+
+Let me know if you have any questions.*/
 $("#query").keyup(function() {
   console.log("Working with query: " + $("#query").val());
   var url = "http://colelyman.com:8080/gc?q=" + $("#query").val();
@@ -7,8 +14,8 @@ $("#query").keyup(function() {
     talks = "<ul>";
     $.each(data, function(i, item) {
       talks += "<li>" + "<span class=\"title\">" + data[i].Title + "</span>";
-      talks += "<a href=\"" + data[i].Media[2].URL + ">";
-      talks += "<img src=\"" + data[i].Images[0].URL + "/>" + "</a" + "</li>";
+      talks += "<a href=\"" + data[i].Media[2].URL + "\">";
+      talks += "<img src=\"" + data[i].Images[0].URL + "\"/>" + "</a>" + "</li>";
     });
     $("#returnStuff").html(talks);
     talks += "</ul>";
