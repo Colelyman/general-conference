194ac2e731df41f3174f1bafde7a6a2d5d5e0e3c
Cole Lyman <cole@colelyman.com>
2015-10-21 12:35:39 -0600

    Added index.html and script.js. Testing API

---
+++ src/script.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+$("#query").keyup(function() {
+  console.log("Working with query: " + $("#query").val());
+  var url = "http://colelyman.com:8080/gc?q=" + $("#query").val();
+  $.getJSON(url, function(data) {
+    var talks;
+    talks = "<ul>";
+    $.each(data, function(i, item) {
+      talks += "<li>" + "<span class=\"title\">" + data[i].Title + "</span>";
+      talks += "<a href=\"" + data[i].Media[2].URL;
+      talks += "<img src=\"" + data[i].Images[0].URL + "/>" + "</a" + "</li>";
+    });
+    $("#returnStuff").html(talks);
+    talks += "</ul>";
+  });
+});
